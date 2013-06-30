be185716878ba215837f1a7cda1768a2ce5f5751
Marshall Levin <mlevin@navigationarts.com>
2013-06-29 23:56:12 -0400

    diffstat - fixed sha1
    Had the wrong sha1 checksum.

--- diffstat.rb
+++ diffstat.rb
@@ -4,7 +4,7 @@ class Diffstat < Formula
   homepage 'http://invisible-island.net/diffstat/'
   url 'http://invisible-island.net/datafiles/release/diffstat.tar.gz'
   version '1.56'
-  sha1 '034c3a72e5d06f5433cdba8c1b6d25146dd699d1'
+  sha1 'c6d9247b8b32eeab5d866686f91a8fbdde3e340f'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
